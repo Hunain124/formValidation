febcf5cdd92a482909cbfe2cd053b07d3c08f3fe
Hunain Shahid <hunainshahid124@gmail.com>
2025-09-10 04:57:57 +0500

    Create a form js project

---
+++ validation.js
@@ -0,0 +1,9 @@
+const form = document.querySelector("#form");
+let userName = document.querySelector("#username");
+let email = document.querySelector("#email");
+let password = document.querySelector("#password");
+let confirmPassword = document.querySelector("#confirmPassword");
+
+form.addEventListener("submit", function (e){
+    e.preventDefault();
+})
