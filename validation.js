4f4790700d0001432ecd178eab2d52a39e617b97
hunain shahid <hunainshahid032@gmail.com>
2025-09-11 01:05:58 +0500

    Add field functionality on form

--- validation.js
+++ validation.js
@@ -4,6 +4,53 @@ let email = document.querySelector("#email");
 let password = document.querySelector("#password");
 let confirmPassword = document.querySelector("#confirmPassword");
 
+// My functions
+
+// error function
+function througherror(input,message){
+    let formControl = input.parentElement;
+    formControl.className = "formControl error";
+    let small = formControl.querySelector("small");
+    small.textContent = message;
+}
+
+// noissue function 
+function noIssue (input){
+    let formControl = input.parentElement;
+    formControl.className = "formControl success";
+}
+
+//confirm password
+function checkPassword(input,message){
+    let formControl = input.parentElement;
+    formControl.className = "formControl error";
+    let small = formControl.querySelector("small");
+    small.textContent = message;
+}
+
+// addEventListener
 form.addEventListener("submit", function (e){
     e.preventDefault();
+    if(userName.value == ""){
+        througherror(userName,"Username is required");
+    }else{
+        noIssue(userName);
+    };
+     if(email.value == ""){
+        througherror(email,"Email is required");
+    }else{
+        noIssue(email);
+    };
+     if(password.value == ""){
+        througherror(password,"Password is required");
+    }else{
+        noIssue(password);
+    };
+     if(confirmPassword.value == ""){
+        througherror(confirmPassword,"Confirm Password is required");
+    }else if(confirmPassword.value != password.value){
+        checkPassword(confirmPassword,"Password is not match");
+    }else{
+        noIssue(confirmPassword);
+    };
 })
