d40168f1d6111a3874b2e706f2e5ad2cb00b2fc3
hunain shahid <hunainshahid032@gmail.com>
2025-09-12 05:43:39 +0500

    Add animation by using GSAB

--- validation.js
+++ validation.js
@@ -33,7 +33,7 @@ function checkLength(input, min, max) {
     if (input.value.length < min) {
         througherror(input, `${input.id} must be above in ${min} letters`);
     } else if (input.value.length > max) {
-        througherror(input, `${input.id} must be in ${min} to ${max} lettes`)
+        througherror(input, `${input.id} must be between ${min} to ${max} letters`)
     }
 
 }
@@ -66,3 +66,32 @@ form.addEventListener("submit", function (e) {
     checkLength(userName, 3, 10);
     checkLength(password, 6, 30)
 })
+
+// Animation for Gsap
+let tl = gsap.timeline();
+
+tl.from(".form", {
+  duration: 1.2,
+  opacity: 0,
+  scale: 0.7,
+  ease: "elastic.out(1, 0.5)"
+})
+.from(".form h2", {
+  duration: 1,
+  y: -50,
+  opacity: 0,
+  ease: "back.out(1.7)"
+}, "-=0.5") // overlap thoda
+.from(".formControl", {
+  duration: 0.8,
+  y: 40,
+  opacity: 0,
+  stagger: 0.2,
+  ease: "power3.out"
+}, "-=0.3")
+gsap.fromTo(".form button",
+  { y: 50, opacity: 0 },   // start
+  { duration: 1, y: 0, opacity: 1, delay: 1.5, ease: "power3.out" } // end
+);
+
+
